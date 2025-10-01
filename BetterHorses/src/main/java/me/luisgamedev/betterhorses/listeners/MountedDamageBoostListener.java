cec8ed0a91cb4396af183fc69039566a790d817c
Luis-Dev <luis.ljate@gmail.com>
2025-10-01 14:55:51 +0200

    Update MountedDamageBoostListener.java

--- BetterHorses/src/main/java/me/luisgamedev/betterhorses/listeners/MountedDamageBoostListener.java
+++ BetterHorses/src/main/java/me/luisgamedev/betterhorses/listeners/MountedDamageBoostListener.java
@@ -6,12 +6,13 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class MountedDamageBoostListener implements Listener {
 
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     public void onMountedDamage(EntityDamageByEntityEvent event) {
         Entity damager = event.getDamager();
         if (!(damager instanceof Player player)) return;
